5605691dde3206f3b04e2502248c95e37076b86c
kaolss <karin@kobotolo.se>
2015-03-30 11:21:31 +0200

    Mobile menu

--- js/responsive-menu.js
+++ js/responsive-menu.js
@@ -3,8 +3,8 @@ jQuery(function( $ ){
 	$("header .genesis-nav-menu, .nav-primary .genesis-nav-menu").addClass("responsive-menu").before('<div class="responsive-menu-icon"></div>');
 
 	$(".responsive-menu-icon").click(function(){
-		$(this).next("header .genesis-nav-menu, .nav-primary .genesis-nav-menu").slideToggle();
-	});
+		$(this).next(".genesis-nav-menu, .nav-primary ").slideToggle();
+            });
 
 	$(window).resize(function(){
 		if(window.innerWidth > 800) {
@@ -14,11 +14,11 @@ jQuery(function( $ ){
 	});
 
 	$(".responsive-menu > .menu-item").click(function(event){
-		if (event.target !== this)
-		return;
-			$(this).find(".sub-menu:first").slideToggle(function() {
-			$(this).parent().toggleClass("menu-open");
-		});
-	});
+            if (event.target !== this)
+            return;
+                $(this).find(".sub-menu:first").slideToggle(function() {
+                $(this).parent().toggleClass("menu-open");                
+            });
+	return false;});
 
 });
